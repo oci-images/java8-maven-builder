be65cb7071e4c2045d0f173e339d9a5230ad1653
Demonio Azteka <rickyrg@gmail.com>
2021-11-21 16:08:13 -0600

    init repo

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# java8-maven-builder
-OCI Image for OpenJDK8 Maven Build
+# Adoptium Temurin OpenJDK8 - OCI Image Maven Builder
+Custom Images for Java8 Maven Builder for Tekton Task
+
+Image build with default charset _en_US.UTF-8_
+
+WORKDIR ***/workspace/source***
+
+ENTRYPOINT _/bin/bash_
+
+CMD _mvn_builder.sh_
+
+|Environment Vars|Description|
+|---|---|
+|MVN_ARGS| set adicional maven args: _-Djava.net.preferIPv4Stack=true_|
+|MAVEN_SSL| set maven.wagon.http.ssl.insecure by default: _true_|
+|MAVEN_GOALS| set maven goals for compiling by fedault: _clean install_|
+
+
+### Docker run
+```
+docker run --env MAVEN_SSL="true" -v SOURCE_FOLDER:/workspace/source demonioazteka/jdk8-maven-builder:latest 
+```
---
+++ mvn_builder.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+echo "Maven Builder Adoptium Temurin OpenJDK8"
+
+export MAVEN_ARGS="-e -B -DskipTests -Djava.net.preferIPv4Stack=true -Dmaven.repo.local=/opt/maven/.m2 -s /configuration/settings.xml --log-file=/workspace/source/build.log"
+if [ ! -z "$MVN_ARGS" ]
+then	
+	export MAVEN_ARGS="$MAVEN_ARGS $MVN_ARGS"
+fi
+
+if [ -z "$MVN_SSL" ]
+then	
+	export MAVEN_SSL="-Dmaven.wagon.http.ssl.insecure=false"
+else	
+	export MAVEN_SSL="-Dmaven.wagon.http.ssl.insecure=$MVN_SSL"
+fi
+
+if [ -z "$MVN_GOALS" ]
+then	
+	export MAVEN_GOALS="clean install"
+else	
+	export MAVEN_GOALS="$MVN_GOALS"
+fi
+
+echo "INFO exec mvn $MAVEN_ARGS $MAVEN_SSL $MAVEN_GOALS"
+mvn $MAVEN_ARGS $MAVEN_SSL $MAVEN_GOALS
+echo "View logs in build.log"
+cat /workspace/source/build.log
